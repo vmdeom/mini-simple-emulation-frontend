397c40b30bbefcc01568a44fc1a4cd940aaebd12
Vitor <vitormourao.dev@outlook.com>
2025-11-29 20:47:25 -0300

    add canvas logic

--- canvas/scripts.js
+++ canvas/scripts.js
@@ -1,3 +1,12 @@
-function main(){
-    console.log("hello world")
-}
+const box = document.getElementById('card-grid');
+
+
+document.addEventListener("keydown", function(e){
+    console.log(e.key)
+    if(e.key == "a"){
+        const card = document.createElement('div');
+        card.classList.add('c-card');   
+        box.appendChild(card)
+    }
+})
+
