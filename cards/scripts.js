1a28e5eeb7dd974bb7aeabf4db27707f57efda0f
Vitor <vitormourao.dev@outlook.com>
2025-11-29 18:04:52 -0300

    add canvas page

---
+++ cards/scripts.js
@@ -0,0 +1,44 @@
+const card = document.querySelector('.card');
+
+card.addEventListener('mouseenter', function(e){
+  const dir = getDirection(e, this);
+
+  this.classList.remove("from-left", "from-right", "from-top", "from-bottom", "center");
+  
+  this.classList.add(`from-${dir}`);
+});
+
+card.addEventListener("mouseleave", function(e){
+
+    this.classList.add("center");
+    console.log("asd")
+    this.classList.remove("from-left", "from-right", "from-top", "from-bottom");
+
+})
+
+
+function getDirection(event, element) {
+  const rect = element.getBoundingClientRect();
+
+  // centro do elemento
+  const xCenter = rect.left + rect.width / 2;
+  const yCenter = rect.top + rect.height / 2;
+
+  // posição do mouse
+  const x = event.clientX - xCenter;
+  const y = event.clientY - yCenter;
+
+  // calcular ângulo
+  const angle = Math.atan2(y, x) * (180 / Math.PI);
+
+  // converter ângulo para direção
+  if (angle >= -45 && angle < 45) {
+    return "right";
+  } else if (angle >= 45 && angle < 135) {
+    return "bottom";
+  } else if (angle >= -135 && angle < -45) {
+    return "top";
+  } else {
+    return "left";
+  }
+}
