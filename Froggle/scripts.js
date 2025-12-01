ae5ba95eb91b0f3c2af12dfbfb17207524f62a21
Vitor <vitormourao.dev@outlook.com>
2025-12-01 07:10:26 -0300

    create menu sketch

---
+++ Froggle/scripts.js
@@ -0,0 +1,43 @@
+const main = document.getElementById("main");
+const title = document.getElementById("title");
+const games = document.getElementById("games")
+
+//const baseGames = document.querySelectorAll(".game");
+//console.log(baseGames)
+counter = [0, 0, 0, 0]
+
+let index = 0
+
+for (let i = 0; i < 4; i++) {
+    let gameDiv = document.createElement("div");
+    games.appendChild(gameDiv)
+    gameDiv.classList.add(`color-${i}`, 'game');
+
+    console.log(i)
+}
+
+const selectChild = games.children
+
+function keyNavigation() {
+
+    document.addEventListener('keypress', (e) => {
+        const keyName = e.key;
+
+        if (keyName === "d") {
+            selectChild[index].classList.remove("selected")
+
+            index = (index + 1) % selectChild.length;
+
+            selectChild[index].classList.add("selected")
+        }
+        else if(keyName === "a"){
+            selectChild[index].classList.remove("selected")
+
+            index = (index + -1 + selectChild.length) % selectChild.length;
+
+            selectChild[index].classList.add("selected")
+        }
+    })
+}
+
+keyNavigation()
