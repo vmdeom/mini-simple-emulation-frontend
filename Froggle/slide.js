b3d9a45a381b4ef348f5f22b8c43f0b0bad3d91a
Vitor <vitormourao.dev@outlook.com>
2025-12-02 09:37:31 -0300

    new slider logic

--- Froggle/slide.js
+++ Froggle/slide.js
@@ -8,14 +8,56 @@ function atualizarSlide() {
     slider[0].style.transform = `translateY(-${slideIndex * height}px)`;
 }
 
+var arr = ["a", "b", "c", "d", "e", "f"]
+let arrIndex = 0
+
+tempArr = Array.from(images)
+console.log(tempArr)
+
+function resetCards(key){
+
+    last = tempArr.shift();
+    tempArr.push(last)
+
+}
+
+function resetSlide(key){
+    if(key == "w"){
+        if(tempArr.length < arr.length){
+            tempArr.push(arr[arrIndex])
+            arrIndex += 1
+            console.log(tempArr)
+        }
+        else if(tempArr.length = arr.length){
+            last = tempArr.shift()
+            tempArr.push(last)
+            console.log(tempArr)
+        }
+    }
+    else if(key == "s"){
+            if(tempArr.length < arr.length){
+            tempArr.push(arr[arrIndex])
+            arrIndex += 1
+            console.log(tempArr)
+        }
+        else if(tempArr.length = arr.length){
+            first = tempArr.pop()
+            tempArr.unshift(first)
+            console.log(tempArr)
+        }
+    }
+}
+
 document.addEventListener("keydown", (e) => {
     if (e.key === "w") {
         slideIndex = (slideIndex - 1 + images.length) % images.length;
+        resetSlide("w")
         atualizarSlide();
     }
 
     if (e.key === "s") {
         slideIndex = (slideIndex + 1) % images.length;
+        resetSlide("s")
         atualizarSlide();
     }
 });
