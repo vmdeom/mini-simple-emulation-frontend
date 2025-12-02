2049e3edd504b0e8d80ac76e3de7dad41b3d109d
Vitor <vitormourao.dev@outlook.com>
2025-12-02 13:50:29 -0300

    new playground

---
+++ test2/scripts.js
@@ -0,0 +1,44 @@
+const scroller = document.getElementById("scroller");
+const slider = document.getElementById("slider");
+const cards = document.getElementsByClassName("card");
+
+let cardsArray = Array.from(cards);
+let slideIndex = 0;
+
+
+function updateSlide(index) {
+    const height = cards[0].clientHeight;
+    console.log(slider)
+    
+    slider.style.transform = `translateY(-${index * height}px)`;
+
+}
+
+function slideCards(key, i){
+    if(key === "s"){
+        let last = cardsArray.shift();
+        //cardsArray.push(last);
+        //slider.appendChild(last)
+        updateSlide(i)
+    }
+    else if(key === "w"){
+        let first = cardsArray.pop();
+        //cardsArray.unshift(first);
+        //slider.prepend(first)
+        updateSlide(i)
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "w"){
+        slideIndex = (slideIndex - 1 + cards.length) % cards.length;
+        slideCards(e.key, slideIndex)
+    }
+    
+    if(e.key === "s")
+    {
+        slideIndex = (slideIndex + 1) % cards.length;
+        slideCards(e.key, slideIndex)
+
+    }
+})
